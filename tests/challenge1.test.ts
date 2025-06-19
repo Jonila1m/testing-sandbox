2709602e84f3dc1c2c6d39a18f5a4008c92aa9b7
Jonila-Murtezi <jonila1murtezi@gmail.com>
2025-06-19 14:14:47 +0200

    Initial commit

---
+++ tests/challenge1.test.ts
@@ -0,0 +1,36 @@
+import { expect, test } from "vitest";
+import { fizzBuzz } from "./challenge1";
+
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+// N will never be less than 1.
+
+test("case 1", () => {
+  const expected = [
+    1,
+    2,
+    "Fizz",
+    4,
+    "Buzz",
+    "Fizz",
+    7,
+    8,
+    "Fizz",
+    "Buzz",
+    11,
+    "Fizz",
+    13,
+    14,
+    "FizzBuzz",
+  ];
+  expect(fizzBuzz(15)).toStrictEqual(expected);
+});
+test("case 2", () => {
+  const expected = [1, 2, "Fizz"];
+  expect(fizzBuzz(3)).toStrictEqual(expected);
+});
---
+++ tests/challenge1.ts
@@ -0,0 +1,19 @@
+// export const addTwoNumbers = (a: number, b: number): number => {
+//   return a + b;
+// };
+
+export const fizzBuzz = (n: number): (number | string)[] => {
+  const result: (number | string)[] = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+};
